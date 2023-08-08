76131678fe045b08484073380641f12b2ecbfe06
tyler smallwood <tylersmall2012@gmail.com>
2023-08-08 15:48:59 +0900

    Started working on Math Game, added the slots for question numbers and answer. Also added Input field.

---
+++ Assets/Scripts/MathGame/MathGame.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MathGame : MonoBehaviour
+{
+
+    [SerializeField] private TMP_Text _question1, _question2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
